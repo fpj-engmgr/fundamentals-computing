4853be525d923d4a6e6f6eb0385c8692ea13f0a1
Frank Jansen <fphjansen@gmail.com>
2024-04-06 18:44:53 -0400

    Working on Fifteen solution

--- PoC/Fifteen/fifteen.py
+++ PoC/Fifteen/fifteen.py
@@ -131,8 +131,31 @@ class Puzzle:
         at the given position in the bottom rows of the puzzle (target_row > 1)
         Returns a boolean
         """
-        # replace with your code
-        return False
+        # is tile zero located at target_row, target_col? if not return False
+        if self.get_number(target_row, target_col) != 0:
+#            print "(", target_row,", ", target_col,") is not tile Zero!"
+            return False
+        # is target_row not the last one?
+        if target_row + 1 >= self.get_height():
+            return False
+        # are tiles to the right of tile zero in the correct position?
+        puzzle_width = self.get_width()
+        #
+        for tile_col in range(target_col + 1, puzzle_width):
+            expected_val = tile_col + target_row * puzzle_width
+#            print "expected_val : ", expected_val
+            if self.get_number(target_row, tile_col) != expected_val:
+#                print "(", target_row,", ", tile_col,") is not ", expected_val
+                return False
+        # are the tiles in target_row + 1 in the correct location?
+        for tile_col in range(puzzle_width):
+            expected_val = tile_col + (target_row + 1) * puzzle_width
+#            print "expected_val : ", expected_val
+            if self.get_number(target_row + 1, tile_col) != expected_val:
+#                print "(", target_row,", ", tile_col,") is not ", expected_val
+                return False
+        # we passed all the tests, so the invariant is True
+        return True
 
     def solve_interior_tile(self, target_row, target_col):
         """
@@ -207,6 +230,31 @@ class Puzzle:
         return ""
 
 # Start interactive simulation
-poc_fifteen_gui.FifteenGUI(Puzzle(4, 4))
+# poc_fifteen_gui.FifteenGUI(Puzzle(4, 4))
 
+"""
+Test section 
+- do some basic tests first
+- lower_row_invariant
+"""
 
+def test_puzzle():
+    # set up a defined puzzle
+    grid_0 = [[4, 2, 3, 7],
+              [8, 5, 6, 10],
+              [9, 1, 0, 11],
+              [12, 13, 14, 15]]
+    height_0 = 4
+    width_0 = 4
+    puzzle_0 = Puzzle(height_0, width_0, grid_0)
+    # check a couple of basic items
+    print puzzle_0
+    assert puzzle_0.get_height() == height_0, "Height assertion failure!"
+    assert puzzle_0.get_width() == width_0, "Width assertion failure!"
+    assert puzzle_0.get_number(2, 2) == 0, "Tile zero not at (2, 2)"
+    #
+    assert puzzle_0.lower_row_invariant(2, 2), "lower_row_invariant failure of (2, 2)"
+    assert not puzzle_0.lower_row_invariant(1, 2), "lower_row_invariant failure of (1, 2)"
+
+#
+test_puzzle()
--- PoC/Tantrix/tantrix.py
+++ PoC/Tantrix/tantrix.py
@@ -11,7 +11,6 @@ a yellow, red or  blue loop of length 10
 """
 
 
-
 # Core modeling idea - a triangular grid of hexagonal tiles are 
 # model by integer tuples of the form (i, j, k) 
 # where i + j + k == size and i, j, k >= 0.
@@ -32,12 +31,10 @@ def reverse_direction(direction):
     return (direction + num_directions / 2) % num_directions
 
 
-
 # Color codes for ten tiles in Tantrix Solitaire
 # "B" denotes "Blue", "R" denotes "Red", "Y" denotes "Yellow"
 SOLITAIRE_CODES = ["BBRRYY", "BBRYYR", "BBYRRY", "BRYBYR", "RBYRYB",
                 "YBRYRB", "BBRYRY", "BBYRYR", "YYBRBR", "YYRBRB"]
-
 
 # Minimal size of grid to allow placement of 10 tiles
 MINIMAL_GRID_SIZE = 4
@@ -54,47 +51,57 @@ class Tantrix:
         Create a triangular grid of hexagons with size + 1 tiles on each side.
         """
         assert size >= MINIMAL_GRID_SIZE
-        pass
+        self._tiling_size = size
 
         # Initialize dictionary tile_value to contain codes for ten
         # tiles in Solitaire Tantrix in one 4x4 corner of grid
-        pass
+        self._tile_value = {}
+        counter = 0        
+        for index_i in range(MINIMAL_GRID_SIZE):
+            for index_j in range(MINIMAL_GRID_SIZE - index_i):
+                index_k = self._tiling_size - (index_i + index_j)
+                grid_index = (index_i, index_j, index_k)
+                self.place_tile(grid_index, SOLITAIRE_CODES[counter])
+                counter += 1
 
     def __str__(self):
         """
         Return string of dictionary of tile positions and values
         """
-        return ""
+        return str(self._tile_value)
         
     def get_tiling_size(self):
         """
         Return size of board for GUI
         """
-        return 0
+        return self._tiling_size
     
     def tile_exists(self, index):
         """
         Return whether a tile with given index exists
         """
-        return False
+        return self._tile_value.has_key(index)
     
     def place_tile(self, index, code):
         """
         Play a tile with code at cell with given index
         """
-        pass       
+        self._tile_value[index] = code       
 
     def remove_tile(self, index):
         """
         Remove a tile at cell with given index
-        and return the code value for that tile        """
-        pass
+        and return the code value for that tile
+        """
+        return self._tile_value.pop(index)
                
     def rotate_tile(self, index):
         """
         Rotate a tile clockwise at cell with given index
         """
-        pass
+        value = self._tile_value[index]
+        new_value = value[-1] + value[:-1] 
+        self._tile_value[index] = new_value
 
     def get_code(self, index):
         """
@@ -106,19 +113,54 @@ class Tantrix:
         """
         Return the index of the tile neighboring the tile with given index in given direction
         """
-        return ()
+        neighbor_offset = DIRECTIONS[direction]
+        neighbor_index = tuple([index[dim] + neighbor_offset[dim] for dim in range(3)])
+        return neighbor_index
 
     def is_legal(self):
         """
         Check whether a tile configuration obeys color matching rules for adjacent tiles
         """
-        return False
+        
+        for tile_index in self._tile_value.keys():
+            tile_code = self._tile_value[tile_index]
+            for direction in DIRECTIONS.keys():
+                neighbor_index = self.get_neighbor(tile_index, direction)
+                if self.tile_exists(neighbor_index):
+                    neighbor_code = self.get_code(neighbor_index)
+                    if tile_code[direction] != neighbor_code[reverse_direction(direction)]:
+                        return False
+        return True
             
     def has_loop(self, color):
         """
         Check whether a tile configuration has a loop of size 10 of given color
         """
-        return False
+        if not self.is_legal():
+            return False        
+        
+        # choose arbitrary starting point
+        tile_indices = self._tile_value.keys()
+        start_index = tile_indices[0]
+        start_code = self._tile_value[start_index]
+        next_direction = start_code.find(color)
+        next_index = self.get_neighbor(start_index, next_direction)
+        current_length = 1
+   
+        # loop through neighboring tiles that match given color
+        while start_index != next_index:
+            current_index = next_index
+            if not self.tile_exists(current_index):
+                return False
+            current_code = self._tile_value[current_index]
+            if current_code.find(color) == reverse_direction(next_direction):
+                next_direction = current_code.rfind(color)
+            else:
+                next_direction = current_code.find(color)
+            next_index = self.get_neighbor(current_index, next_direction)
+            current_length += 1
+      
+        return current_length == len(SOLITAIRE_CODES)
 
     
 # run GUI for Tantrix
