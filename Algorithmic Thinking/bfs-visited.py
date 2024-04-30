326b56907e8d0c3cdb904cd4889fca76523e8770
Frank Jansen <fphjansen@gmail.com>
2024-04-30 14:42:49 -0400

    complete connected components. start on app #2

--- Algorithmic Thinking/bfs-visited.py
+++ Algorithmic Thinking/bfs-visited.py
@@ -1,6 +1,9 @@
+"""
+Basic functions to perform network analysis
+"""
 from collections import deque
 
-def bfs_visited(ugraph: dict, start_node):
+def bfs_visited(ugraph, start_node):
     """
     Function to compute the set of connected nodes in an undirected graph that 
     are visited when starting a breadth-first search at start_node.
@@ -26,7 +29,7 @@ def bfs_visited(ugraph: dict, start_node):
     
     return visited
 
-def cc_visited(ugraph: dict):
+def cc_visited(ugraph):
     """
     Function to get a list of all connected nodes within an undirected graph.
 
@@ -43,11 +46,13 @@ def cc_visited(ugraph: dict):
         node = remain_nodes[0]
         visited = bfs_visited(ugraph, node)
         c_components.append(visited)
-        remain_nodes.pop(0)
+        for visit in visited:
+            if visit in remain_nodes:
+                remain_nodes.remove(visit)
     #
     return c_components
 
-def largest_cc_size(ugraph: dict):
+def largest_cc_size(ugraph):
     """
     Function to return the largest number of connected nodes in an undirected graph
 
@@ -66,7 +71,7 @@ def largest_cc_size(ugraph: dict):
     #
     return max_cc_size
 
-def compute_resilience(ugraph: dict, attack_order: list):
+def compute_resilience(ugraph, attack_order):
     """
     Function to comupute the resilience of a network to removal of nodes in attack_order
 
@@ -94,79 +99,79 @@ def compute_resilience(ugraph: dict, attack_order: list):
                 edges.remove(attack_node)
         #
         network_max_cc.append(largest_cc_size(un_graph))
-        print("c_r: ", un_graph)
+        # print("c_r: ", un_graph)
     #
     return network_max_cc
 
 # 3 example graphs to use for analysis and testing
 #
-EX_GRAPH0 = {0: set([1, 2]),
-             1: set([]),
-             2: set([])}
+#EX_GRAPH0 = {0: set([1, 2]),
+#             1: set([0, 2]),
+#             2: set([0, 1])}
+##
+#EX_GRAPH1 = {0: set([1, 4, 5]),
+#             1: set([2, 6]),
+#             2: set([3]),
+#             3: set([]),
+#             4: set([1]),
+#             5: set([2]),
+#             6: set([])}
+##
+#EX_GRAPH2 = {0: set([1, 4, 5]),
+#             1: set([2, 6]),
+#             2: set([3, 7]),
+#             3: set([]),
+#             4: set([1]),
+#             5: set([2]),
+#             6: set([]),
+#             7: set([3]),
+#             8: set([1, 2]),
+#             9: set([0, 3, 4, 5, 6, 7])}
 #
-EX_GRAPH1 = {0: set([1, 4, 5]),
-             1: set([2, 6]),
-             2: set([3]),
-             3: set([]),
-             4: set([1]),
-             5: set([2]),
-             6: set([])}
 #
-EX_GRAPH2 = {0: set([1, 4, 5]),
-             1: set([2, 6]),
-             2: set([3, 7]),
-             3: set([]),
-             4: set([1]),
-             5: set([2]),
-             6: set([]),
-             7: set([3]),
-             8: set([1, 2]),
-             9: set([0, 3, 4, 5, 6, 7])}
-
-
-# bf_visited tests
-st_node = 0
-v0 = bfs_visited(EX_GRAPH0, st_node)
-print("EX_GRAPH0 [", st_node, "] : ", v0)
-st_node = 0
-v1 = bfs_visited(EX_GRAPH1, st_node)
-print("EX_GRAPH1 [", st_node, "] : ", v1)
-st_node = 4
-v1 = bfs_visited(EX_GRAPH1, st_node)
-print("EX_GRAPH1 [", st_node, "] : ", v1)
-st_node = 0
-v2 = bfs_visited(EX_GRAPH2, st_node)
-print("EX_GRAPH2 [", st_node, "] : ", v2)
-st_node = 2
-v2 = bfs_visited(EX_GRAPH2, st_node)
-print("EX_GRAPH2 [", st_node, "] : ", v2)
-st_node = 6
-v2 = bfs_visited(EX_GRAPH2, st_node)
-print("EX_GRAPH2 [", st_node, "] : ", v2)
-st_node = 9
-v2 = bfs_visited(EX_GRAPH2, st_node)
-print("EX_GRAPH2 [", st_node, "] : ", v2)
+## bf_visited tests
+#st_node = 0
+#v0 = bfs_visited(EX_GRAPH0, st_node)
+#print("EX_GRAPH0 [", st_node, "] : ", v0)
+#st_node = 0
+#v1 = bfs_visited(EX_GRAPH1, st_node)
+#print("EX_GRAPH1 [", st_node, "] : ", v1)
+#st_node = 4
+#v1 = bfs_visited(EX_GRAPH1, st_node)
+#print("EX_GRAPH1 [", st_node, "] : ", v1)
+#st_node = 0
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+#st_node = 2
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+#st_node = 6
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+#st_node = 9
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+##
+## cc_visited tests
+#cc0 = cc_visited(EX_GRAPH0)
+#cc1 = cc_visited(EX_GRAPH1)
+#cc2 = cc_visited(EX_GRAPH2)
+##
+#print("EX_GRAPH0 : ", cc0)
+#print("EX_GRAPH1 : ", cc1)
+#print("EX_GRAPH2 : ", cc2)
 #
-# cc_visited tests
-cc0 = cc_visited(EX_GRAPH0)
-cc1 = cc_visited(EX_GRAPH1)
-cc2 = cc_visited(EX_GRAPH2)
+## largest_cc_size tests
+#ls0 = largest_cc_size(EX_GRAPH0)
+#ls1 = largest_cc_size(EX_GRAPH1)
+#ls2 = largest_cc_size(EX_GRAPH2)
+##
+#print("EX_GRAPH0 largest : ", ls0)
+#print("EX_GRAPH1 largest : ", ls1)
+#print("EX_GRAPH2 largest : ", ls2)
 #
-print("EX_GRAPH0 : ", cc0)
-print("EX_GRAPH1 : ", cc1)
-print("EX_GRAPH2 : ", cc2)
-
-# largest_cc_size tests
-ls0 = largest_cc_size(EX_GRAPH0)
-ls1 = largest_cc_size(EX_GRAPH1)
-ls2 = largest_cc_size(EX_GRAPH2)
+##
+#att1 = [1, 6, 4]
+#res1 = compute_resilience(EX_GRAPH1, att1)
 #
-print("EX_GRAPH0 largest : ", ls0)
-print("EX_GRAPH1 largest : ", ls1)
-print("EX_GRAPH2 largest : ", ls2)
-
-#
-att1 = [1, 6, 4]
-res1 = compute_resilience(EX_GRAPH1, att1)
-
-print("EX_GRAPH1 : ", res1, att1, EX_GRAPH1)
+#print("EX_GRAPH1 : ", res1, att1, EX_GRAPH1)
