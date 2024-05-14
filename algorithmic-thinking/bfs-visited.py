d82ce8486150a968b4a776ea80cdbaa547e84cb6
Frank Jansen <fphjansen@gmail.com>
2024-05-13 20:26:11 -0400

    folder rename

---
+++ algorithmic-thinking/bfs-visited.py
@@ -0,0 +1,177 @@
+"""
+Basic functions to perform network analysis
+"""
+from collections import deque
+
+def bfs_visited(ugraph, start_node):
+    """
+    Function to compute the set of connected nodes in an undirected graph that 
+    are visited when starting a breadth-first search at start_node.
+
+    Args:
+        ugraph (dict): Undirected graph
+        start_node (int): Starting node for the search
+    Returns:
+        visited (set): Set of nodes that were visited during the search
+    """
+    queue = deque()
+    #
+    visited = {start_node}
+    #
+    queue.append(start_node)
+    #
+    while len(queue) > 0:
+        current_node = queue.popleft()
+        for neighbor in ugraph[current_node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+    
+    return visited
+
+def cc_visited(ugraph):
+    """
+    Function to get a list of all connected nodes within an undirected graph.
+
+    Args:
+        ugraph (dict): Undirected graph
+    Returns:
+        c_components (list): List of all sets that are connected to a node
+    """
+    remain_nodes = list(ugraph.keys())
+    #
+    c_components = list()
+    #
+    while len(remain_nodes) > 0:
+        node = remain_nodes[0]
+        visited = bfs_visited(ugraph, node)
+        c_components.append(visited)
+        for visit in visited:
+            if visit in remain_nodes:
+                remain_nodes.remove(visit)
+    #
+    return c_components
+
+def largest_cc_size(ugraph):
+    """
+    Function to return the largest number of connected nodes in an undirected graph
+
+    Args:
+        ugraph (graph): Undirected graph
+    Returns:
+        max_cc_size (int): Count of nodes in largest set of connected components
+    """
+    max_cc_size = 0
+    #
+    conn_components = cc_visited(ugraph)
+    #
+    for cc_nodes in conn_components:
+        if len(cc_nodes) > max_cc_size:
+            max_cc_size = len(cc_nodes)
+    #
+    return max_cc_size
+
+def compute_resilience(ugraph, attack_order):
+    """
+    Function to comupute the resilience of a network to removal of nodes in attack_order
+
+    Args:
+        ugraph (dict): Undirected graph
+        attack_order (list): List of nodes that are attacked in each iteration
+    Returns:
+        network_max_cc (list): List of sizes of the largest number of connected
+            after each attack 
+    """
+    # make a copy of ugraph (so we don't mess with it)
+    un_graph = ugraph.copy()
+    # First entry is the current state of connectivity
+    network_max_cc = []
+    #
+    network_max_cc.append(largest_cc_size(un_graph))
+    #
+    for attack_node in attack_order:
+        # remove the node
+        un_graph.pop(attack_node)
+        # step through the remaining ugraph nodes to remove edges to attack_node
+        for node in un_graph.keys():
+            edges = un_graph[node]
+            if attack_node in edges:
+                edges.remove(attack_node)
+        #
+        network_max_cc.append(largest_cc_size(un_graph))
+        # print("c_r: ", un_graph)
+    #
+    return network_max_cc
+
+# 3 example graphs to use for analysis and testing
+#
+#EX_GRAPH0 = {0: set([1, 2]),
+#             1: set([0, 2]),
+#             2: set([0, 1])}
+##
+#EX_GRAPH1 = {0: set([1, 4, 5]),
+#             1: set([2, 6]),
+#             2: set([3]),
+#             3: set([]),
+#             4: set([1]),
+#             5: set([2]),
+#             6: set([])}
+##
+#EX_GRAPH2 = {0: set([1, 4, 5]),
+#             1: set([2, 6]),
+#             2: set([3, 7]),
+#             3: set([]),
+#             4: set([1]),
+#             5: set([2]),
+#             6: set([]),
+#             7: set([3]),
+#             8: set([1, 2]),
+#             9: set([0, 3, 4, 5, 6, 7])}
+#
+#
+## bf_visited tests
+#st_node = 0
+#v0 = bfs_visited(EX_GRAPH0, st_node)
+#print("EX_GRAPH0 [", st_node, "] : ", v0)
+#st_node = 0
+#v1 = bfs_visited(EX_GRAPH1, st_node)
+#print("EX_GRAPH1 [", st_node, "] : ", v1)
+#st_node = 4
+#v1 = bfs_visited(EX_GRAPH1, st_node)
+#print("EX_GRAPH1 [", st_node, "] : ", v1)
+#st_node = 0
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+#st_node = 2
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+#st_node = 6
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+#st_node = 9
+#v2 = bfs_visited(EX_GRAPH2, st_node)
+#print("EX_GRAPH2 [", st_node, "] : ", v2)
+##
+## cc_visited tests
+#cc0 = cc_visited(EX_GRAPH0)
+#cc1 = cc_visited(EX_GRAPH1)
+#cc2 = cc_visited(EX_GRAPH2)
+##
+#print("EX_GRAPH0 : ", cc0)
+#print("EX_GRAPH1 : ", cc1)
+#print("EX_GRAPH2 : ", cc2)
+#
+## largest_cc_size tests
+#ls0 = largest_cc_size(EX_GRAPH0)
+#ls1 = largest_cc_size(EX_GRAPH1)
+#ls2 = largest_cc_size(EX_GRAPH2)
+##
+#print("EX_GRAPH0 largest : ", ls0)
+#print("EX_GRAPH1 largest : ", ls1)
+#print("EX_GRAPH2 largest : ", ls2)
+#
+##
+#att1 = [1, 6, 4]
+#res1 = compute_resilience(EX_GRAPH1, att1)
+#
+#print("EX_GRAPH1 : ", res1, att1, EX_GRAPH1)
---
+++ algorithmic-thinking/citation-graph.py
@@ -0,0 +1,168 @@
+"""
+Provided code for Application portion of Module 1
+
+Imports physics citation graph 
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+# Omit this section for right now
+
+# general imports
+import urllib2
+
+# Set timeout for CodeSkulptor if necessary
+import codeskulptor
+codeskulptor.set_timeout(20)
+
+# end of codeskulptor items
+"""
+###################################
+# Code for loading citation graph
+
+# CITATION_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_phys-cite.txt"
+CITATION_URL = "/Users/fpj/Development/python/fundamentals-computing/Algorithmic Thinking/data/alg_phys-cite.txt"
+
+def load_graph(graph_url):
+    """
+    Function that loads a graph given the URL
+    for a text representation of the graph
+    
+    Returns a dictionary that models a graph
+    """
+#    graph_file = urllib2.urlopen(graph_url)
+    graph_file = open(graph_url)
+    graph_text = graph_file.read()
+    graph_lines = graph_text.split('\n')
+    graph_lines = graph_lines[ : -1]
+    
+    print("Loaded graph with", len(graph_lines), "nodes")
+    
+    answer_graph = {}
+    for line in graph_lines:
+        neighbors = line.split(' ')
+        node = int(neighbors[0])
+        answer_graph[node] = set([])
+        for neighbor in neighbors[1 : -1]:
+            answer_graph[node].add(int(neighbor))
+
+    return answer_graph
+
+def compute_in_degrees(digraph):
+    """
+    Function to calculate the in-degrees for each node in a digraph
+    
+    Returns a dict of nodes and their in-degrees
+    """
+    # start with an empty in-degree dictionary
+    in_degree_dict = {}
+    # go through the nodes in digraph and set in-degree to 0
+    for node in digraph.keys():
+        in_degree_dict[node] = len(digraph[node])
+    # now got through the edges in digraph and count them
+    #for edges in digraph.values():
+    #    for node in edges:
+    #        in_degree_dict[node] += 1
+    #
+    return in_degree_dict
+    
+def in_degree_distribution(digraph):
+    """
+    Function to calculate the number of nodes with a specific in-degree
+    
+    Returns dict with in-degree key and node count value
+    """
+    # first compute the in-degrees for each node in the digraph
+    in_degree_dict = compute_in_degrees(digraph)
+    # create an empty degree distribution dict
+    in_degree_dist_dict = {}
+    # go through each node and look at its in-degrees
+    for node in in_degree_dict.keys():
+        in_degrees = in_degree_dict[node]
+        # if this in-degree count exists increment, else set to 1
+        if in_degrees in in_degree_dist_dict:
+            in_degree_dist_dict[in_degrees] += 1
+        else:
+            in_degree_dist_dict[in_degrees] = 1
+    # return the distribution
+    return in_degree_dist_dict
+
+def total_in_degrees(digraph):
+    """
+    Helper function to calculate the total number of in-degrees
+
+    Args:
+        digraph (dict): directed graph to be traversed
+    """
+    #
+    totindeg = 0
+    #
+    for node_key in digraph.keys():
+        totindeg += node_key * digraph[node_key]
+    #
+    return totindeg
+   
+def normal_in_degree_distribution(digraph):
+    """
+    Function to return a normalized in-degree distribution for a given
+    directed graph
+    
+    Returns a dict with normalized in-degree values
+    """
+    # get the number of nodes in the digraph
+    num_nodes = float(len(digraph))
+    # get the in-degree distribution (not normalized)
+    in_degree_distro = in_degree_distribution(digraph)
+    #
+    normal_in_degree_dist_dict = {}
+    # step through the in-degree distribution and normalize
+    for node_key in in_degree_distro.keys():
+        # get the value, normalize and store
+        normal_in_degree_dist_dict[node_key] = float(in_degree_distro[node_key]) / num_nodes
+    #
+    return normal_in_degree_dist_dict
+
+def plot_digraph_loglog(digraph):
+    """
+    Function to plot a normalized digraph in loglog mode
+    """
+    # Let's organize the digraph into x and y data
+    points_list = sorted(digraph.items())
+    # remove 0 in_degrees from the points_list to avoid log(0)
+    del points_list[0]
+    #
+    x, y = zip(*points_list)
+    #
+    # Now let's set up the plotting area
+    fig, ax = plt.subplots(figsize=(16, 10))
+    fig.suptitle('Log/Log plot of in_degree distribution for the citation graph')
+    ax.scatter(x, y)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('Number of Citations')
+    ax.set_ylabel('Fraction of Papers')
+    plt.show()
+    
+    
+    
+citation_graph = load_graph(CITATION_URL)
+#print(len(citation_graph))
+in_degree_dist = in_degree_distribution(citation_graph)
+print(in_degree_dist)
+
+totlindegs = total_in_degrees(in_degree_dist)
+print('Length of citation graph', len(citation_graph))
+print('Total in-degrees : ', totlindegs)
+print('Average in-degrees : ', totlindegs / len(citation_graph))
+
+#print(in_degree_dist)
+normal_in_degree = normal_in_degree_distribution(citation_graph)
+sorted_digraph = sorted(normal_in_degree.items())
+#del sorted_digraph[0]
+#print(sorted_digraph)
+print(len(normal_in_degree), len(in_degree_dist))
+
+plot_digraph_loglog(normal_in_degree)
+
+
